6293cc27d616aa1fcffbbf21310de94b456a8b9c
MUKILTEO\1720986 <Kirby336242@gmail.com>
2024-12-02 12:53:58 -0800

    Incomplete target but have body

--- 2D Template/Assets/Settings/Scripts/Turret.cs
+++ 2D Template/Assets/Settings/Scripts/Turret.cs
@@ -2,15 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 
 public class Turret : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private Transform turretRotationPoint;
+    [SerializeField] private LayerMask enemyMask;
 
     [Header("Attribute")]
     [SerializeField] private float targetingRange = 5f;
+    private Transform target;
+    private void Update()
+    {
+        if (target == null)
+        {
+            FindTarget();
+        }
+        {
+            { }
+             
+        } 
 
+
+    }
+    private void FindTarget()
+    {
+        RaycastHit2D[] raycastHit2Ds = Physics2D.CircleCastAll(transform.position, targetingRange, (Vector2)transform.position, 0f, enemyMask);
+        
+        if (hits.Length > 0)
+        {
+            target = hits[0].transform;
+        }
+);
+    }
     private void OnDrawGizmosSelected()
     {
         Handles.color = Color.cyan;
@@ -24,9 +49,6 @@ public class Turret : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
+    
 }
